8742ebfe235e70f570904dd3b2fb868afe6ef750
David Herdoiza Morales <david.herdoiza@hpe.com>
2016-03-03 00:45:34 +0000

    new: dev: Add no shutdown for LAG in show run interfaces
    The command show running-config interface should print if a
    lag interface is in no shutdown
    
    Change-Id: I0ccb219d6532514dcf5c2d60e3aae1b5afd44a4f
    Signed-off-by: David Herdoiza Morales <david.herdoiza@hpe.com>

--- src/cli/intf_vty.c
+++ src/cli/intf_vty.c
@@ -1206,6 +1206,11 @@ parse_lag(struct vty *vty)
             /* Print the LAG port name because lag port is present. */
             vty_out (vty, "interface lag %s%s", &port_row->name[LAG_PORT_NAME_PREFIX_LENGTH], VTY_NEWLINE);
 
+            data = port_row->admin;
+            if(data && strcmp(data, OVSREC_PORT_ADMIN_UP) == 0) {
+                vty_out(vty, "%3s%s%s", "", "no shutdown", VTY_NEWLINE);
+            }
+
             if (check_port_in_bridge(port_row->name))
             {
                 vty_out (vty, "%3s%s%s", "", "no routing", VTY_NEWLINE);
