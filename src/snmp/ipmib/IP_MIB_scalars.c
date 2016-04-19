812bbc9ecd60eeedcdd6a997e8a7206ab0eaca40
Pooja Kulkarni <pooja.kulkarni@hpe.com>
2016-04-18 23:41:53 -0700

    new: dev: Added ifTable and ifXTable support in IF-MIB
    Added IP-MIB support.(ipIfStatsTable, ipSystemStatsTable )
    
    Change-Id: Ifc88d7fe7c5f7b3c01d2e8735a10c7848c569cf1
    Signed-off-by: Pooja Kulkarni <pooja.kulkarni@hpe.com>

---
+++ src/snmp/ipmib/IP_MIB_scalars.c
@@ -0,0 +1,16 @@
+//
+// Net-SNMP MIB module IP-MIB (http://pysnmp.sf.net)
+// ASN.1 source file:///ws/varmaav/snmp/src/ops-snmpd/mibs/IP-MIB.txt
+// Produced by pysmi-0.0.6 at Tue Apr 19 04:38:54 2016
+// On host hpnsw905.rose.rdlabs.hpecorp.net platform Linux version
+// 3.16.0-37-generic by user varmaav
+// Using Python version 2.7.6 (default, Jun 22 2015, 17:58:13)
+//
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "IP_MIB_custom.h"
+#include "IP_MIB_scalars.h"
+#include "IP_MIB_scalars_ovsdb_get.h"
+#include "ovsdb-idl.h"
+#include "vswitch-idl.h"
